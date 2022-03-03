533947e9fa02e34bdc6056fb0755ad63f1df7291
nijesh1999 <87565112+nijesh1999@users.noreply.github.com>
2022-03-03 11:13:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import Github from './Components/github';
+import Index from "./components/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Index />
+      {/* <Github />  */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar1 from "./navbar";
+
+import Maincontent from "./mainContent";
+
+import "../css/style.css";
+
+const Index = () => {
+  return (
+    <div>
+      <Navbar1 />
+      <Maincontent />
+    </div>
+  );
+};
+
+export default Index;
